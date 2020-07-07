827de41e684ccec9a857e58cbad896cfe1a14346
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-07-07 12:40:58 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,18 @@
+def reverseParentheses(s):
+    stack = []
+    reverse = []
+        
+    for i in range(len(s)):
+        if s[i] == ')':  # pop the substring between nearest parentheses
+            p = stack.pop()
+            while p != '(':
+                reverse.append(p)
+                p = stack.pop()
+            stack += reverse
+            reverse = []
+        else:
+            stack.append(s[i])
+    return ''.join(stack)
+
+s = input()
+print(reverseParentheses(s))
